b6e00bf71ddf6acfdb93fe3eb1396d9e15871170
kunalmanishdesai <kunalmanishdesai@yahoo.com>
2023-03-25 18:40:58 +0530

    Added @Repository

--- src/main/java/com/Wario/WaaS/Transaction/Repository/TransactionRepository.java
+++ src/main/java/com/Wario/WaaS/Transaction/Repository/TransactionRepository.java
@@ -4,11 +4,13 @@ import com.Wario.WaaS.Transaction.Entity.Transaction;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TransactionRepository extends PagingAndSortingRepository<Transaction,Long>, JpaRepository<Transaction,Long> {
 
-    List<Transaction> findAllByToIdentifierOrFromIdentifier(long toIdentifier, long fromIdentifier, Pageable pageable);
+    List<Transaction> findAllByToIdentifierOrFromIdentifier(String toIdentifier, String fromIdentifier, Pageable pageable);
 
 }
