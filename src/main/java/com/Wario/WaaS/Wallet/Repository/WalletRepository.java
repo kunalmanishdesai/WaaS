28b5e83a05737f49ff38b9d31f4845e56b25e42a
kunalmanishdesai <kunalmanishdesai@yahoo.com>
2023-03-25 18:43:44 +0530

    Adding @Repository

--- src/main/java/com/Wario/WaaS/Wallet/Repository/WalletRepository.java
+++ src/main/java/com/Wario/WaaS/Wallet/Repository/WalletRepository.java
@@ -2,9 +2,11 @@ package com.Wario.WaaS.Wallet.Repository;
 
 import com.Wario.WaaS.Wallet.Entity.WalletEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface WalletRepository extends JpaRepository<WalletEntity,Long> {
 
    Optional<WalletEntity> findByCompanyIdAndUserId(String companyId, String userId);
