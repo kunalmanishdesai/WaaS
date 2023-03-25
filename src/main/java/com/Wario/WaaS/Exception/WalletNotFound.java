d6b7efb285e726bd5a7be689d0cbd1ba0759f531
kunalmanishdesai <kunalmanishdesai@yahoo.com>
2023-03-25 18:43:17 +0530

    Adding new exception --> wallet not found

--- src/main/java/com/Wario/WaaS/Exception/WalletNotFound.java
+++ src/main/java/com/Wario/WaaS/Exception/WalletNotFound.java
@@ -1,10 +1,14 @@
 package com.Wario.WaaS.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class WalletNotFound extends RuntimeException {
 
-    private static final String errorMessageTemplate = "Wallet not found for user Id: %s" +
-            "for companyId: %s";
-    public WalletNotFound(String companyId,String userId) {
-        super(String.format(errorMessageTemplate,userId,companyId));
+    private static final String walletNotFound = "Wallet not found";
+
+    public WalletNotFound() {
+        super(walletNotFound);
     }
 }
